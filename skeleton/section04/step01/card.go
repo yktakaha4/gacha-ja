f3162a7f6bda7438d59a46cfd3e981758786630f
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2021-07-11 17:42:47 +0900

    add step01

--- skeleton/section04/step01/card.go
+++ skeleton/section04/step01/card.go
@@ -1,3 +1,30 @@
 package main
 
-// TODO: rarityやcardに関する処理をここに移す
+// rarityやcardに関する処理をここに移す
+
+// -- card.goに移す ここから --
+type rarity string
+
+const (
+	rarityN  rarity = "N"
+	rarityR  rarity = "R"
+	raritySR rarity = "SR"
+	rarityXR rarity = "XR"
+)
+
+func (r rarity) String() string {
+	return string(r)
+}
+
+type card struct {
+	rarity rarity // レア度
+	name   string // 名前
+}
+
+func (c *card) String() string {
+	// レア度:名前のように文字列を作る
+	// 例："SR:ドラゴン"
+	return c.rarity.String() + ":" + c.name
+}
+
+// -- card.goに移す ここまで --
