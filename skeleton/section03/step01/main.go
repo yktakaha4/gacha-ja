19a02cccef681bf66451cba642c5c00c8b6061c5
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2021-07-11 14:25:12 +0900

    add step01

--- skeleton/section03/step01/main.go
+++ skeleton/section03/step01/main.go
@@ -13,26 +13,28 @@ func main() {
 	// 現在時刻をUNIX時間にしたものを種とする
 	rand.Seed(time.Now().Unix())
 
-	// TODO: 以下の処理をdraw関数として定義する
+	// 以下の処理をdraw関数として定義する
+	draw := func () {
+		// 0から99までの間で乱数を生成する
+		num := rand.Intn(100)
 
-	// 0から99までの間で乱数を生成する
-	num := rand.Intn(100)
-
-	// 変数numが0〜79のときは"ノーマル"、
-	// 80〜94のときは"R"、95〜98のときは"SR"、
-	// それ以外のときは"XR"と表示する
-	switch {
-	case num < 80:
-		fmt.Println("ノーマル")
-	case num < 95:
-		fmt.Println("R")
-	case num < 99:
-		fmt.Println("SR")
-	default:
-		fmt.Println("XR")
+		// 変数numが0〜79のときは"ノーマル"、
+		// 80〜94のときは"R"、95〜98のときは"SR"、
+		// それ以外のときは"XR"と表示する
+		switch {
+		case num < 80:
+			fmt.Println("ノーマル")
+		case num < 95:
+			fmt.Println("R")
+		case num < 99:
+			fmt.Println("SR")
+		default:
+			fmt.Println("XR")
+		}
 	}
 
 	//  -- ここまで
 
-	// TODO: draw関数を呼ぶ
+	// draw関数を呼ぶ
+	draw()
 }
