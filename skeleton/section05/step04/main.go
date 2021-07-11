257d5e56f9246f89b70aa8483724e80737029aae
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2021-07-11 21:03:48 +0900

    add step04

--- skeleton/section05/step04/main.go
+++ skeleton/section05/step04/main.go
@@ -27,9 +27,10 @@ func initialTickets() int {
 		os.Exit(1)
 	}
 
-	// TODO: プログラム引数の1つめを取得し
+	// プログラム引数の1つめを取得し
 	// strconv.Atoi関数でint型に変換する
 	// 第1戻り値は変数num、第2戻り値は変数errに代入する
+	num, err := strconv.Atoi(os.Args[1])
 
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
